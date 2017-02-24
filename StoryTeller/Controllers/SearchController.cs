e0d5937d3379c9f40b973e367808fd044093680a
Kirill <K.Yatsenko@tt.com.pl>
2017-02-24 09:41:26 +0100

    Implemented SearchUser, added SearchController

---
+++ StoryTeller/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using StoryTeller.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoryTeller.Controllers
+{
+    [Authorize]
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public SearchController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View(db.Users);
+        }
+
+        public ActionResult SearchPeople(string searchText)
+        {
+            var term = searchText.ToLower();
+            var result = db.Users
+                .Where(p =>
+                    p.StoryTellerName.ToLower().Contains(term)
+                );
+            return PartialView("~/Views/Search/Partial/_SearchStoryTellerUsers.cshtml", result);
+        }
+    }
+}
